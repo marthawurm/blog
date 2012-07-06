0b7dc6845ee64a662056e8f7ae519243be1205ba
Njeri Chelimo <martha.c.chumo@gmail.com>
2012-07-06 17:48:04 +0300

    HOPELESS

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'b7dcb4036d938109861b31db2967650548704481f4e32d9732bb425eedb1fd7fe67fe6b6c6c22e200b332e3c02b08c9618a022f8c33955012aa858a87fad70d8'
